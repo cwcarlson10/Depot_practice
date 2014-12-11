4c8641d35f19bd3f9801c899878d0b183f9478f9
Chris Carlson <cwcarlson10@gmail.com>
2014-12-11 09:13:44 -0700

    set up email env

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Depot::Application.configure do
+  config.action_mailer.deliver_method = :test
+end
